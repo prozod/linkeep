bc1436c02ac6cee47d7d5203c8c55c87e653ec74
t0ux <wandrease@gmail.com>
2022-07-09 18:25:59 +0300

    fixed padding (testing lazygit)

--- frontend/src/components/Sidebar/sidebar.styles.ts
+++ frontend/src/components/Sidebar/sidebar.styles.ts
@@ -2,5 +2,5 @@ export const styles = {
   defaults: ['bg-slate-900'],
   title: ['flex items-center justify-between py-2 px-4'],
   line: ['h-[2px] w-full bg-slate-700'],
-  items: ['py-16 px-2'],
+  items: ['py-2 px-2'],
 };
