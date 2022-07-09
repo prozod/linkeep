515656ffc47ae0747747322984ec3212c0bcf6fc
t0ux <wandrease@gmail.com>
2022-07-09 21:50:54 +0300

    changes on main

--- frontend/src/components/Sidebar/sidebar.styles.ts
+++ frontend/src/components/Sidebar/sidebar.styles.ts
@@ -2,5 +2,5 @@ export const styles = {
   defaults: ['bg-slate-900'],
   title: ['flex items-center justify-between py-2 px-4'],
   line: ['h-[2px] w-full bg-slate-700'],
-  items: ['py-20 px-2'],
+  items: ['py-4 px-2'],
 };
